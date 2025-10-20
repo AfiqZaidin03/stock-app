1be579d3b5828eb0e1dbbc1e83d7cc97a38cdf72
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2025-10-20 17:58:05 +0800

    feat: implement SignUpPage with form handling and InputField component

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,82 @@
-import React from "react";
+"use client";
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
 
 const SignUpPage = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      country: "US",
+      investmentGoals: "Growth",
+      riskTolerance: "Medium",
+      preferredIndustry: "Technology",
+    },
+
+    mode: "onBlur",
+  });
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log("Form Data Submitted:", data);
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="form-title">Sign Up & Personalize</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        {/* INPUTS */}
+        <InputField
+          name="fullName"
+          label="Full Name"
+          placeholder="Enter your full name"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: "Full Name is required", minLength: 2 }}
+        />
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="contact@gmail.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email is required",
+            pattern: /^\S+@\S+$/i,
+            message: "Email address is required",
+          }}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter your password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLength: 8 }}
+        />
+
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Creating account" : " Start Your Investing Journey"}
+        </Button>
+      </form>
+    </>
+  );
 };
 
 export default SignUpPage;
+
+//stop at 1:32:30
